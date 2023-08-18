156aa65df782801a65da7f9a7ecb26a55be82f2e
Asus <suyashadhikari99@gmail.com>
2023-08-18 15:58:06 +0545

    Student Table Initialized

--- src/main/java/com/college/management/config/InitDbConfig.java
+++ src/main/java/com/college/management/config/InitDbConfig.java
@@ -2,22 +2,26 @@ package com.college.management.config;
 
 import com.college.management.entity.Role;
 import com.college.management.entity.Student;
+import com.college.management.entity.User;
 import com.college.management.repo.RoleRepo;
+import com.college.management.repo.StudentRepo;
 import com.college.management.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
 public class InitDbConfig {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final StudentRepo studentRepo;
 
     @PostConstruct
-    public void addRoleData(){
-        //Adding Roles if Roles Table Empty
+    public void addData(){
+        //Initializing Roles if Roles Table Empty
         if (roleRepo.findAll().size()==0){
             //Creating System Admin Role
             Role adminRole = new Role();
@@ -35,6 +39,23 @@ public class InitDbConfig {
             Role savedTeacher =  roleRepo.save(teacherRole);
         }
 
+        //Initializing Test-Student if Students table empty
+        if (studentRepo.findAll().size()==0){
+            Student student = new Student();
+            student.setFirstName("Test");
+            student.setLastName("Student");
+            student.setAddress("Kathmandu");
+            student.setRollNo(0);
+            student.setBatch(2001);
+            User user = new User();
+            String username = student.getBatch().toString()+student.getRollNo().toString();
+            user.setUserName(username);
+            user.setPassword("test");
+            user.setRoles(Arrays.asList(roleRepo.getByRole("student")));
+            student.setUser(user);
+            userRepo.save(user);
+            studentRepo.save(student);
 
+        }
     }
 }
